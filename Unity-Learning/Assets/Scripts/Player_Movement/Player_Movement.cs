849df6d5d0408bd2230342fb2530e56952f82827
Martin Mallon <88795187+noodle-soups@users.noreply.github.com>
2023-04-30 15:38:06 +0100

    Player Movement
    Horizontal & vertical movement added

--- Unity-Learning/Assets/Scripts/Player_Movement/Player_Movement.cs
+++ Unity-Learning/Assets/Scripts/Player_Movement/Player_Movement.cs
@@ -2,23 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class move_test : MonoBehaviour
+public class Player_Movement : MonoBehaviour
 {
 
     Rigidbody2D rb;
+
     float move_speed;
     float input_h;
     float input_v;
-
-
 
     // Start is called before the first frame update
     void Start()
     {
 
         rb = GetComponent<Rigidbody2D>();
-        move_speed = 5.0f;
 
+        move_speed = 10.0f;
+        
     }
 
     // Update is called once per frame
@@ -27,16 +27,17 @@ public class move_test : MonoBehaviour
 
         input_h = Input.GetAxisRaw("Horizontal");
         input_v = Input.GetAxisRaw("Vertical");
-        
+
+
         if (input_h != 0)
         {
-            rb.AddForce(new Vector2(input_h * move_speed, 0f));
-        }
-        
-        if (input_v != 0)
-        {
-            rb.AddForce(new Vector2(input_v * move_speed, 0));
+            rb.AddForce(new Vector2(move_speed * input_h, 0));
         }
 
+        if (input_v != 0)
+        {
+            rb.AddForce(new Vector2(0, move_speed * input_v));
+        }
+        
     }
 }
